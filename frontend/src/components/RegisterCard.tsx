fcb8f40a207b774981bb9ffc2fc86c45d0326913
José Villalobos <jl.vj169@gmail.com>
2026-06-25 11:51:31 -0500

    feat: add RegisterCard component

---
+++ frontend/src/components/RegisterCard.tsx
@@ -0,0 +1,51 @@
+interface RegisterCardProps {
+  num: number;
+  abiName: string;
+  value: number;
+  signed: boolean;
+  onToggle: () => void;
+}
+
+export default function RegisterCard({
+  num,
+  abiName,
+  value,
+  signed,
+  onToggle,
+}: RegisterCardProps) {
+  const displayValue = signed ? value | 0 : value;
+
+  return (
+    <div
+      className={`flex flex-row justify-between items-center p-2 border border-blue-500 font-mono shadow-2xs text-white ${num === 0 ? "bg-blue-950" : "bg-blue-800"}`}
+    >
+      <div className="flex flex-row gap-2 w-24">
+        <span className="font-bold">x{num}</span>
+        <span>({abiName})</span>
+      </div>
+      <div className="flex flex-row gap-2">
+        <span className="font-bold">
+          0x{value.toString(16).toUpperCase().padStart(8, "0")} ({displayValue})
+        </span>
+
+        <div className="flex items-center gap-2 ml-4">
+          <span className="text-xs text-blue-200">
+            {signed ? "Sig" : "UnS"}
+          </span>
+          <button
+            onClick={onToggle}
+            className={`relative inline-flex h-5 w-9 items-center rounded-full transition-colors focus:outline-none ${
+              signed ? "bg-indigo-900" : "bg-blue-400"
+            }`}
+          >
+            <span
+              className={`inline-block h-3 w-3 transform rounded-full bg-white transition-transform ${
+                signed ? "translate-x-5" : "translate-x-1"
+              }`}
+            />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
