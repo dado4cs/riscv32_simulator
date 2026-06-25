bea7cb9a2c746d4f2e5fc8309baa74b58b41272d
José Villalobos <jl.vj169@gmail.com>
2026-06-25 12:48:29 -0500

    feat: add Dashboard component

---
+++ frontend/src/components/Dashboard.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useRef, useState } from "react";
+import init, { Cpu } from "risc_v_simulator";
+import RegisterFile from "./RegisterFile";
+import Console from "./Console";
+
+export default function Dashboard() {
+  const [cpuReady, SetCpuReady] = useState<boolean>(false);
+  const [registers, setRegisters] = useState(new Array(32).fill(0));
+  const [logs, setLogs] = useState<string[]>([]);
+  const cpuRef = useRef<Cpu | null>(null);
+
+  useEffect(() => {
+    const initCpu = async () => {
+      await init();
+      const cpu = new Cpu(1024 * 4);
+      cpuRef.current = cpu;
+      const initialRegs = new Array(32)
+        .fill(0)
+        .map((_, i) => cpu.get_register(i));
+
+      setRegisters(initialRegs);
+      setLogs(["[System]: Risc-V-Simulator Ready"]);
+      SetCpuReady(true);
+    };
+    initCpu();
+  }, []);
+
+  return (
+    <div className="flex h-screen flex-col justify-between bg-gray-800">
+      <RegisterFile registers={registers} />
+      <Console logs={logs} />
+    </div>
+  );
+}
