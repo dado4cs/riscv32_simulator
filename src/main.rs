b57e8b49c2c9a3b8c3cbb09e01c9514a5b1c4033
José Villalobos <jl.vj169@gmail.com>
2026-06-24 21:58:26 -0500

    feat: add cli menu for step, registers and memory view

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::env;
 use std::fs;
+use std::io::{self, Write};
+use std::str::FromStr;
 use risc_v_simulator::Cpu;
 
 fn main() {
@@ -24,6 +26,55 @@ fn main() {
     let mut cpu = Cpu::new(1024 * 4);
     cpu.load_program(&program);
     loop {
-        cpu.step();
+        println!("\nOptions: \n 1. step \n 2. show_register \n 3. show_memory \n 4. exit");
+        print!("Select an option: ");
+        io::stdout().flush().unwrap();
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Error reading input");
+        let option: usize = usize::from_str(input.trim()).unwrap_or(0); 
+
+        match option {
+            1 => {
+                cpu.step();
+                println!("Executed 1 step.");
+            }
+            2 => {
+                print!("Enter register number (0-31): ");
+                io::stdout().flush().unwrap();
+                
+                input.clear();
+                io::stdin().read_line(&mut input).expect("Error");
+                let register: usize = usize::from_str(input.trim()).unwrap_or(99);
+                
+                if register < 32 {
+                    let register_value = cpu.get_register(register);
+                    println!("x{}: {} (0x{:08X})", register, register_value as i32, register_value);
+                } else {
+                    println!("Invalid register!");
+                }
+            }
+            3 => {
+                print!("Enter memory address: ");
+                io::stdout().flush().unwrap();
+                
+                input.clear();
+                io::stdin().read_line(&mut input).expect("Error");
+                let memory_address: usize = usize::from_str(input.trim()).unwrap_or(usize::MAX);
+                
+                if memory_address < cpu.memory.len() {
+                    let memory_value = cpu.get_memory_value(memory_address);
+                    println!("Address {}: {} (0x{:02X})", memory_address, memory_value, memory_value);
+                } else {
+                    println!("Memory address out of bounds!");
+                }
+            }
+            4 => {
+                println!("Exiting emulator...");
+                break;
+            }
+            _ => {
+                println!("Incorrect option");
+            }
+        }
     }
 }
