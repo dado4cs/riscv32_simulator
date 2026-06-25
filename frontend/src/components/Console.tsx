c7fe6e5f9310b236d688552f58be48e5736affee
José Villalobos <jl.vj169@gmail.com>
2026-06-25 01:17:03 -0500

    feat: add Console component

---
+++ frontend/src/components/Console.tsx
@@ -0,0 +1,26 @@
+interface ConsoleLogs {
+  logs: string[];
+}
+
+export default function Console({ logs }: ConsoleLogs) {
+  return (
+    <div className="bg-black shadow-2xs h-24 font-mono text-zinc-400 flex flex-col gap-1.5 p-4 overflow-y-auto rounded-lg border-zinc-600">
+      {logs.length === 0 ? (
+        <span>Waiting for program</span>
+      ) : (
+        logs.map((log, idx) => (
+          <div key={idx}>
+            <span className="text-green-400 font-bold">{"> "}</span>
+            <span
+              className={
+                log.includes("Exit code") ? "text-indigo-400" : "text-zinc-400"
+              }
+            >
+              {log}
+            </span>
+          </div>
+        ))
+      )}
+    </div>
+  );
+}
